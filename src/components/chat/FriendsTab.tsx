26bbcb7a7ca5688d7675fa8d9c623f0c5c82e852
ayus-10 <136055230+ayus-10@users.noreply.github.com>
2024-11-27 15:24:45 +0545

    Extend FriendsTab component's functionality

--- src/components/chat/FriendsTab.tsx
+++ src/components/chat/FriendsTab.tsx
@@ -1,9 +1,10 @@
 import Image from "next/image";
-import { useState } from "react";
+import { FormEvent, useMemo, useRef, useState } from "react";
 import { FaCheck } from "react-icons/fa6";
 import { IoSearch } from "react-icons/io5";
 import { MdClose } from "react-icons/md";
 import { PiUserCirclePlusThin } from "react-icons/pi";
+import allUserProfiles from "../../assets/dummy_profiles.json";
 
 const SENT = "sent";
 const RECIEVED = "recieved";
@@ -13,42 +14,79 @@ type ActiveTab = typeof SENT | typeof RECIEVED;
 
 export default function FriendsTab() {
   const [activeTab, setActiveTab] = useState<ActiveTab>(RECIEVED);
-  const [_search, setSearch] = useState("");
+
+  const [search, setSearch] = useState("");
+
+  const searchInputRef = useRef<HTMLInputElement>(null);
+
+  const [profileNotFound, setProfileNotFound] = useState(false);
+
+  const userProfileFound = useMemo(() => {
+    const profile = allUserProfiles.find((p) => p.email === search);
+    if (!profile && search) {
+      setProfileNotFound(true);
+    }
+    return profile;
+  }, [search]);
+
+  function searchUserProfile(e: FormEvent) {
+    e.preventDefault();
+
+    if (!searchInputRef.current) {
+      return;
+    }
+
+    if (search) {
+      clearSearchStates();
+      searchInputRef.current.value = "";
+    } else {
+      setSearch(searchInputRef.current.value.toLowerCase());
+    }
+  }
+
+  function clearSearchStates() {
+    setProfileNotFound(false);
+    setSearch("");
+  }
 
   return (
     <div className="flex h-full flex-col gap-8">
       <div className="flex flex-col gap-2">
         <h1 className="text-lg font-semibold md:text-xl">Find friends</h1>
-        <div className="relative mb-2 w-full">
+        <form className="relative mb-2 w-full" onSubmit={searchUserProfile}>
           <input
-            onChange={(e) => setSearch(e.target.value.toLowerCase())}
+            ref={searchInputRef}
+            onChange={clearSearchStates}
             placeholder="Email..."
             type="text"
             className="w-full rounded-lg bg-white py-2 pl-4 pr-16 shadow-black outline-none duration-200 ease-in-out hover:drop-shadow-md focus:drop-shadow-md dark:bg-gray-750"
           />
           <button className="absolute right-0 top-1/2 flex h-full w-14 -translate-y-1/2 items-center justify-center rounded-lg bg-purple-700 px-2 text-xl text-white duration-200 ease-in-out hover:bg-purple-800">
-            <IoSearch />
+            {search ? <MdClose /> : <IoSearch />}
           </button>
-        </div>
-        <div className="flex h-16 items-center justify-center gap-2 rounded-lg bg-purple-200 px-3 dark:bg-gray-750">
-          <PiUserCirclePlusThin className="flex-shrink-0 text-5xl text-purple-700 dark:text-white" />
-          <h2 className="leading-5 text-purple-700 dark:text-white md:text-lg md:leading-6">
-            Search for friends using email
-          </h2>
-        </div>
+        </form>
+        {userProfileFound ? (
+          <FriendRequestCard tab={FIND} user={userProfileFound} />
+        ) : (
+          <SearchFriendsResult notFound={profileNotFound} />
+        )}
       </div>
       <div className="flex h-full flex-col gap-2">
         <h1 className="text-lg font-semibold md:text-xl">Friend requests</h1>
         <div className="relative flex justify-between gap-2 rounded-lg bg-purple-200 p-2 dark:bg-gray-750">
           <button
             onClick={() => setActiveTab(RECIEVED)}
-            className={`w-full rounded-lg bg-purple-300 px-4 py-1 duration-200 ease-in-out md:px-6 md:py-2 md:font-semibold ${activeTab === RECIEVED ? "text-white" : "text-purple-700"}`}
+            className={`w-full rounded-lg bg-purple-300 px-4 py-1 duration-200 ease-in-out md:px-6 md:py-2 md:font-semibold ${
+              activeTab === RECIEVED ? "text-white" : "text-purple-700"
+            }`}
           >
             <span className="relative z-30">Recieved</span>
           </button>
           <button
             onClick={() => setActiveTab(SENT)}
-            className={`w-full rounded-lg bg-purple-300 px-4 py-1 duration-200 ease-in-out md:px-6 md:py-2 md:font-semibold ${activeTab === SENT ? "text-white" : "text-purple-700"}`}
+            className={`w-full rounded-lg bg-purple-300 px-4 py-1 duration-200 ease-in-out md:px-6 md:py-2 md:font-semibold ${
+              activeTab === SENT ? "text-white" : "text-purple-700"
+            }`}
           >
             <span className="relative z-30">Sent</span>
           </button>
@@ -57,8 +95,8 @@ export default function FriendsTab() {
           ></div>
         </div>
         <div className="my-2 mb-4 flex h-1 grow flex-col gap-2 overflow-y-scroll">
-          {Array.from({ length: 10 }).map((_, index) => (
-            <FriendRequestCard key={`FriendRequest${index}`} tab={activeTab} />
+          {allUserProfiles.map((p) => (
+            <FriendRequestCard key={p.id} tab={activeTab} user={p} />
           ))}
         </div>
       </div>
@@ -66,38 +104,56 @@ export default function FriendsTab() {
   );
 }
 
+function SearchFriendsResult({ notFound }: { notFound: boolean }) {
+  return (
+    <div className="flex h-16 items-center justify-center gap-2 rounded-lg bg-purple-200 px-3 dark:bg-gray-750">
+      <PiUserCirclePlusThin className="flex-shrink-0 text-5xl text-purple-700 dark:text-white" />
+      <h2 className="leading-5 text-purple-700 dark:text-white md:text-lg md:leading-6">
+        {notFound
+          ? "No user found with that email"
+          : "Search for friends using email"}
+      </h2>
+    </div>
+  );
+}
+
 interface FriendRequestCardProps {
   tab: typeof FIND | typeof RECIEVED | typeof SENT;
+  user: {
+    username: string;
+    profilePictureUrl: string;
+    email: string;
+    requestSent: string;
+  };
 }
 
 function FriendRequestCard(props: FriendRequestCardProps) {
-  const { tab } = props;
+  const { tab, user } = props;
 
   return (
-    <div
-      className={`flex items-center gap-2 rounded-lg bg-gray-100 px-3 py-2 dark:bg-gray-750 ${tab === FIND ? "h-16" : "h-auto"}`}
-    >
-      <Image
-        height={50}
-        width={50}
-        src={
-          "https://cdn.discordapp.com/avatars/833573797809750066/2f4530153f0d992c760ff2ba115380b5.webp"
-        }
-        alt="Profile picture"
-        className="size-[50px] rounded-full"
-      />
-      <div className="flex grow flex-col">
-        <h2 className="line-clamp-1 md:text-lg md:font-semibold">
-          Olivia Isabel Rodrigo
-        </h2>
-        <div className="flex items-end justify-between gap-2">
-          <RequestAction tab={tab} />
-          {(tab === RECIEVED || tab === SENT) && (
-            <span className="text-sm text-gray-500 dark:text-gray-400">
-              1 day
-            </span>
-          )}
+    <div className="flex flex-col gap-2 rounded-lg bg-gray-100 px-3 py-2 dark:bg-gray-750">
+      <div className="flex gap-2">
+        <Image
+          height={50}
+          width={50}
+          src={user.profilePictureUrl}
+          alt={user.username}
+          className="size-[50px] rounded-full"
+        />
+        <div>
+          <h2 className="line-clamp-1 md:text-lg md:font-semibold">
+            {user.username}
+          </h2>
+          <h2 className="line-clamp-1 text-sm md:text-base">{user.email}</h2>
         </div>
+      </div>
+      <div className="flex items-end justify-between gap-2">
+        <RequestAction tab={tab} />
+        {tab !== FIND ? (
+          <span className="text-sm text-gray-500 dark:text-gray-400">
+            {user.requestSent}
+          </span>
+        ) : null}
       </div>
     </div>
   );
@@ -135,8 +191,10 @@ function RequestAction({ tab }: RequestActionProps) {
       );
     case FIND:
       return (
-        <button className="rounded-full bg-purple-700 px-2 font-semibold duration-200 ease-in-out hover:bg-purple-800 dark:bg-purple-500 dark:hover:bg-purple-600">
-          Add
+        <button className="rounded-full border-2 border-purple-200 bg-purple-200 px-3 text-purple-500 duration-200 ease-in-out hover:border-purple-500 hover:bg-transparent">
+          <span className="hidden text-sm font-semibold md:inline">
+            Add friend
+          </span>
         </button>
       );
   }
