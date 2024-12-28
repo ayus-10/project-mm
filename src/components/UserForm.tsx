24fe8bf82f14f5b1da3c2d4fe875bba62c4f2786
ayus-10 <136055230+ayus-10@users.noreply.github.com>
2024-12-28 20:55:36 +0545

    add loading state, error message on login/signup form

--- src/components/UserForm.tsx
+++ src/components/UserForm.tsx
@@ -1,4 +1,4 @@
-import { FormEvent, useState } from "react";
+import { FormEvent, useEffect, useState } from "react";
 import { Link } from "react-router-dom";
 import GoogleIcon from "../assets/google.ico";
 import BannerImage from "../assets/account-banner.png";
@@ -6,6 +6,8 @@ import { FaEye } from "react-icons/fa";
 import { LuChevronRight } from "react-icons/lu";
 import { ACCESS_TOKEN } from "../constants";
 import axios from "axios";
+import { BeatLoader } from "react-spinners";
+import { IoMdAlert } from "react-icons/io";
 
 const LOGIN = "LOGIN";
 const SIGNUP = "SIGNUP";
@@ -29,10 +31,23 @@ export default function UserForm({ formType }: UserFormProps) {
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
 
+  const [loading, setLoading] = useState(false);
+  const [errorMessage, setErrorMessage] = useState("");
+
+  useEffect(() => {
+    if (!errorMessage) return;
+
+    const timeoutId = setTimeout(() => setErrorMessage(""), 4000);
+
+    return () => clearTimeout(timeoutId);
+  }, [errorMessage]);
+
   async function handleSubmit(e: FormEvent<HTMLFormElement>) {
     e.preventDefault();
 
     try {
+      setLoading(true);
+
       if (formType === SIGNUP) {
         const payload = {
           Email: email,
@@ -61,14 +76,16 @@ export default function UserForm({ formType }: UserFormProps) {
       }
     } catch (error) {
       if (axios.isAxiosError(error)) {
-        alert(error.response?.data);
+        setErrorMessage(error.response?.data);
       }
+    } finally {
+      setLoading(false);
     }
   }
 
   return (
     <main className="mx-auto flex min-h-screen w-screen max-w-[1000px] items-center px-5 py-4">
-      <div className="flex w-full flex-col md:flex-row md:justify-evenly lg:justify-between">
+      <div className="flex w-full flex-col items-center md:flex-row md:justify-evenly lg:justify-between">
         <div className="md:w-[300px] lg:w-[450px]">
           <div className="my-6 flex flex-col gap-2 text-center md:text-start">
             <h1 className="text-3xl font-semibold text-gray-800">
@@ -93,6 +110,12 @@ export default function UserForm({ formType }: UserFormProps) {
             <p className="text-gray-500">Or use Email</p>
             <div className="h-[1px] grow bg-gray-300"></div>
           </div>
+          {errorMessage ? (
+            <div className="my-2 flex w-full items-center gap-2 rounded-lg bg-red-500 p-2 text-white">
+              <IoMdAlert size={25} className="flex-shrink-0" />
+              {errorMessage}
+            </div>
+          ) : null}
           <form className="my-6 flex flex-col gap-2" onSubmit={handleSubmit}>
             <div className="relative flex flex-col-reverse">
               <input
@@ -152,11 +175,17 @@ export default function UserForm({ formType }: UserFormProps) {
                 />
               ) : null}
             </div>
-            <button className="group flex items-center justify-center rounded-md bg-purple-700 px-3 py-4 text-white">
-              <span className="text-lg">
-                {formType === SIGNUP ? "Sign up" : "Log in"}
-              </span>
-              <LuChevronRight className="relative text-2xl duration-300 ease-in-out group-hover:translate-x-2" />
+            <button className="group flex h-[3.75rem] items-center justify-center rounded-md bg-purple-700 px-3 py-4 text-white">
+              {loading ? (
+                <BeatLoader color="#fff" />
+              ) : (
+                <>
+                  <span className="text-lg">
+                    {formType === SIGNUP ? "Sign up" : "Log in"}
+                  </span>
+                  <LuChevronRight className="relative text-2xl duration-300 ease-in-out group-hover:translate-x-2" />
+                </>
+              )}
             </button>
           </form>
           <p className="my-2 text-center text-sm text-gray-500">
