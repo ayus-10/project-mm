d78b9019c3bbafa75d042aa1d55e814251eaa396
ayus-10 <136055230+ayus-10@users.noreply.github.com>
2025-01-10 14:10:07 +0545

    bug fixes

--- src/components/chat/navigation/tabs/friends/FriendList.tsx
+++ src/components/chat/navigation/tabs/friends/FriendList.tsx
@@ -1,11 +1,12 @@
+import { useContext } from "react";
 import { MoonLoader as Loading } from "react-spinners";
 
 import { IFriend } from "@/interfaces/IFriend";
 import { ViewFriendsTab } from "./types";
 
-import NotFound from "@/assets/empty.png";
-
+import { AuthenticatedUserContext } from "@/contexts/AuthenticatedUserContext";
 import FriendRequestCard from "./FriendCard";
+import NotFound from "@/assets/empty.png";
 
 interface FriendListProps {
   sent?: IFriend[];
@@ -18,6 +19,11 @@ interface FriendListProps {
 export default function FriendList(props: FriendListProps) {
   const { tab, received, sent, all, isLoading } = props;
 
+  const { user: loggedInUser } = useContext(AuthenticatedUserContext);
+
+  const isLoggedInUserReceiver = (f: IFriend) =>
+    f.receiverEmail === loggedInUser.email;
+
   const isValidArray = (arr: IFriend[] | undefined): arr is IFriend[] =>
     Array.isArray(arr) && arr.length > 0;
 
@@ -28,6 +34,15 @@ export default function FriendList(props: FriendListProps) {
   const showSent = isSentTab && isValidArray(sent);
   const showReceived = isReceivedTab && isValidArray(received);
   const showAll = isAllTab && isValidArray(all);
+
+  const errorMessage =
+    isSentTab && !isValidArray(sent)
+      ? "You have not sent any requests yet."
+      : isReceivedTab && !isValidArray(received)
+        ? "You have not received any requests yet."
+        : isAllTab && !isValidArray(all)
+          ? "You have not made any friends yet."
+          : null;
 
   if (isLoading) {
     return (
@@ -64,30 +79,28 @@ export default function FriendList(props: FriendListProps) {
               />
             ))
           : showAll
-            ? all.map((r) => (
+            ? all.map((f) => (
                 <FriendRequestCard
-                  key={r.friendId}
+                  key={f.friendId}
                   tab={tab}
                   user={{
-                    email: r.senderEmail,
-                    fullName: r.senderFullName,
-                    id: r.senderId,
+                    email: isLoggedInUserReceiver(f)
+                      ? f.senderEmail
+                      : f.receiverEmail,
+                    fullName: isLoggedInUserReceiver(f)
+                      ? f.senderFullName
+                      : f.receiverFullName,
+                    id: isLoggedInUserReceiver(f) ? f.senderId : f.receiverId,
                   }}
                 />
               ))
             : null}
-      <div className="flex flex-col items-center py-4">
-        <img className="size-[125px]" src={NotFound} alt="Not found" />
-        <span>
-          {isSentTab && !isValidArray(sent)
-            ? "You have not sent any requests yet."
-            : isReceivedTab && !isValidArray(received)
-              ? "You have not received any requests yet."
-              : isAllTab && !isValidArray(all)
-                ? "You have not made any friends yet."
-                : null}
-        </span>
-      </div>
+      {errorMessage ? (
+        <div className="flex flex-col items-center py-4">
+          <img className="size-[125px]" src={NotFound} alt="Not found" />
+          <span>{errorMessage}</span>
+        </div>
+      ) : null}
     </div>
   );
 }
